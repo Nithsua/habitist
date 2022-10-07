ca0f0f863d1718fcb7adb8a6744cb339ee88e210
Nivas Muthu M G <nivasmuthu452@gmail.com>
2022-10-07 12:30:46 +0530

    [RFCT]: Wrapper

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'dart:async';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:habitist/app/wrapper.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -19,8 +21,22 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Habitist',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: const Scaffold(body: Center(child: Text('Hola'))),
+      theme: Theme.of(context).copyWith(
+        primaryColor: Colors.black,
+        scaffoldBackgroundColor: Colors.white,
+        backgroundColor: Colors.white,
+        appBarTheme: Theme.of(context).appBarTheme.copyWith(
+            backgroundColor: Colors.white, foregroundColor: Colors.black),
+        textTheme: Theme.of(context)
+            .textTheme
+            .apply(fontFamily: GoogleFonts.nunitoSans().fontFamily),
+        bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+            type: BottomNavigationBarType.fixed,
+            showSelectedLabels: false,
+            showUnselectedLabels: false,
+            selectedIconTheme: IconThemeData(color: Colors.white)),
+      ),
+      home: const Wrapper(),
     );
   }
 }
