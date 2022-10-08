51d0eec6ee3c6833ea2d8ae0cb465d336a459daf
Nivas Muthu M G <nivasmuthu452@gmail.com>
2022-10-08 09:43:06 +0530

    [UI]: Working New Habit Screen

--- lib/app/screens/new_habit.screen.dart
+++ lib/app/screens/new_habit.screen.dart
@@ -1,17 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:habitist/app/models/view/new_habit_screen/new_habit_screen.model.dart';
+import 'package:habitist/app/screens/home.screen.dart';
+import 'package:habitist/app/viewmodels/new_habit_screen.viewmodel.dart';
 import 'package:habitist/app/widgets/segmented_control.widget.dart';
 
 class NewHabitScreen extends ConsumerWidget {
-  const NewHabitScreen({Key? key}) : super(key: key);
+  final newHabitScreenProvider =
+      StateNotifierProvider<NewHabitScreenViewModel, NewHabitScreenModel>(
+          (ref) => NewHabitScreenViewModel());
+  NewHabitScreen({Key? key}) : super(key: key);
+
+  Future<void> createHabit(BuildContext context, WidgetRef ref) async {
+    ref.read(newHabitScreenProvider.notifier).createHabit().then((value) {
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => const HomeScreen()));
+    });
+  }
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       body: NestedScrollView(
         headerSliverBuilder: (context, innerBoxIsScrolled) => [
-          const SliverAppBar(
+          SliverAppBar(
             pinned: true,
+            actions: [
+              MaterialButton(
+                height: 20.0,
+                onPressed: ref.watch(newHabitScreenProvider).habit.validate
+                    ? () => createHabit(context, ref)
+                    : null,
+                child: const Text('Save'),
+              )
+            ],
           )
         ],
         body: SingleChildScrollView(
@@ -22,24 +44,83 @@ class NewHabitScreen extends ConsumerWidget {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 TextFieldWithLabel(
-                    label: 'Name Your Habit',
-                    hintText: 'Coffee, Tea, Running',
-                    onChange: (_) {}),
+                  label: 'Name Your Habit',
+                  hintText: 'Coffee, Tea, Running',
+                  onChange:
+                      ref.read(newHabitScreenProvider.notifier).changeName,
+                  controller:
+                      ref.read(newHabitScreenProvider.notifier).nameController,
+                  textInputType: TextInputType.name,
+                ),
                 const SizedBox(height: 12.0),
                 SegmentedControlWithLabel(
                   label: 'Wanna',
                   labels: const ['Gain', 'Lose'],
-                  onChange: (_) {},
+                  selected: ref.watch(newHabitScreenProvider).habit.type.index,
+                  onChange:
+                      ref.read(newHabitScreenProvider.notifier).changeType,
+                  caption:
+                      'Choose the type of habit tracking based on whether you are trying to develop a new habit or get rid a bad one',
+                ),
+                const SizedBox(height: 12.0),
+                SegmentedControlWithLabel(
+                  label: 'Period',
+                  labels: const ['Day', 'Week', 'Month', 'Year'],
+                  selected:
+                      ref.watch(newHabitScreenProvider).habit.period.index,
+                  onChange:
+                      ref.read(newHabitScreenProvider.notifier).changePeriod,
+                  caption: 'Choose the frequency you wanna track the habit by',
+                ),
+                const SizedBox(height: 12.0),
+                TextFieldWithLabel(
+                  label: 'Total Count',
+                  hintText: '3000',
+                  onChange:
+                      ref.read(newHabitScreenProvider.notifier).changeCount,
+                  caption:
+                      'Total quantity you wanna keep track off. like 3000ml of water',
+                  controller: ref
+                      .read(newHabitScreenProvider.notifier)
+                      .totalCountController,
+                  textInputType: TextInputType.number,
+                ),
+                const SizedBox(height: 12.0),
+                TextFieldWithLabel(
+                  label: 'Incremental Count',
+                  hintText: '1000',
+                  onChange: ref
+                      .read(newHabitScreenProvider.notifier)
+                      .changeIncrementalCount,
+                  caption:
+                      'Quantity to increase by for each increment, eg by 1000ml of water for every increment',
+                  controller: ref
+                      .read(newHabitScreenProvider.notifier)
+                      .incrementalController,
+                  textInputType: TextInputType.number,
+                ),
+                const SizedBox(height: 12.0),
+                TextFieldWithLabel(
+                  label: 'Unit',
+                  hintText: 'l, km, cups, etc',
+                  onChange:
+                      ref.read(newHabitScreenProvider.notifier).changeUnit,
+                  controller:
+                      ref.read(newHabitScreenProvider.notifier).unitController,
+                  textInputType: TextInputType.text,
                 ),
                 const SizedBox(height: 12.0),
                 TextFieldWithLabel(
                   label: 'Description',
                   hintText: 'To get better and healthy',
-                  onChange: (_) {},
+                  onChange: ref
+                      .read(newHabitScreenProvider.notifier)
+                      .changeDescription,
+                  controller: ref
+                      .read(newHabitScreenProvider.notifier)
+                      .descriptionController,
+                  textInputType: TextInputType.text,
                 ),
-                const SizedBox(height: 12.0),
-                TextFieldWithLabel(
-                    label: 'Unit', hintText: 'l, g, km, etc', onChange: (_) {}),
               ],
             ),
           ),
@@ -53,8 +134,16 @@ class SegmentedControlWithLabel extends StatelessWidget {
   final String label;
   final List<String> labels;
   final void Function(int)? onChange;
-  const SegmentedControlWithLabel(
-      {super.key, required this.label, required this.labels, this.onChange});
+  final String? caption;
+  final int selected;
+  const SegmentedControlWithLabel({
+    super.key,
+    this.onChange,
+    this.caption,
+    required this.label,
+    required this.labels,
+    required this.selected,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -69,7 +158,17 @@ class SegmentedControlWithLabel extends StatelessWidget {
               ?.copyWith(fontWeight: FontWeight.bold),
         ),
         const SizedBox(height: 8.0),
-        SegmentedControl(labels: labels, onChange: onChange, selected: 0),
+        SegmentedControl(
+            labels: labels, onChange: onChange, selected: selected),
+        caption != null
+            ? Column(
+                children: [
+                  const SizedBox(height: 4),
+                  Text(caption!, style: Theme.of(context).textTheme.caption),
+                  const SizedBox(height: 8)
+                ],
+              )
+            : const SizedBox()
       ],
     );
   }
@@ -80,9 +179,14 @@ class TextFieldWithLabel extends StatelessWidget {
   final TextEditingController? controller;
   final String hintText;
   final void Function(String) onChange;
+  final String? caption;
+  final TextInputType? textInputType;
+
   const TextFieldWithLabel(
       {super.key,
       this.controller,
+      this.caption,
+      this.textInputType,
       required this.label,
       required this.hintText,
       required this.onChange});
@@ -101,12 +205,16 @@ class TextFieldWithLabel extends StatelessWidget {
         ),
         const SizedBox(height: 8.0),
         TextField(
+          keyboardType: textInputType,
           controller: controller,
           style: Theme.of(context).textTheme.bodyMedium?.copyWith(height: 1.0),
           onChanged: onChange,
           decoration: InputDecoration(
             filled: true,
             hintText: hintText,
+            hintStyle: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                  color: Colors.grey,
+                ),
             fillColor: Colors.grey.shade300,
             border: OutlineInputBorder(
               borderRadius: BorderRadius.circular(8.0),
@@ -115,9 +223,17 @@ class TextFieldWithLabel extends StatelessWidget {
             isDense: true,
           ),
           cursorColor: Colors.black,
-          cursorHeight: 15,
           cursorRadius: const Radius.circular(8.0),
         ),
+        caption != null
+            ? Column(
+                children: [
+                  const SizedBox(height: 4),
+                  Text(caption!, style: Theme.of(context).textTheme.caption),
+                  const SizedBox(height: 8)
+                ],
+              )
+            : const SizedBox()
       ],
     );
   }
