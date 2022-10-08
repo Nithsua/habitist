ba820f4130c3acfe25e5db86ec967722287b8c22
Nivas Muthu M G <nivasmuthu452@gmail.com>
2022-10-08 18:50:03 +0530

    [RFCT]: Auth service

---
+++ lib/app/services/auth.service.dart
@@ -0,0 +1,22 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class AuthenticationService {
+  final auth = FirebaseAuth.instance;
+
+  Future<void> signInWithGoogle() async {
+    await GoogleSignIn().signIn().then((googleAccount) async {
+      if (googleAccount != null) {
+        final googleAccountAuthentication = await googleAccount.authentication;
+        final credentials = GoogleAuthProvider.credential(
+            idToken: googleAccountAuthentication.idToken,
+            accessToken: googleAccountAuthentication.accessToken);
+        await auth.signInWithCredential(credentials);
+      }
+    });
+  }
+
+  Future<void> signOut() async {
+    await auth.signOut();
+  }
+}
