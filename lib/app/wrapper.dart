80b855404bc2157d72293022a07f90de3a417cc6
Nivas Muthu M G <nivasmuthu452@gmail.com>
2022-10-08 18:49:35 +0530

    [FEAT]: Auth State listener

--- lib/app/wrapper.dart
+++ lib/app/wrapper.dart
@@ -1,12 +1,54 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:habitist/app/screens/onboard.screen.dart';
+import 'dart:async';
 
-class Wrapper extends ConsumerWidget {
-  const Wrapper({Key? key}) : super(key: key);
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:habitist/app/screens/home.screen.dart';
+import 'package:habitist/app/screens/onboard.screen.dart';
+import 'package:habitist/main.dart';
+
+class Wrapper extends StatefulWidget {
+  const Wrapper({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    return OnboardScreen();
+  State<Wrapper> createState() => _WrapperState();
+}
+
+class _WrapperState extends State<Wrapper> {
+  StreamSubscription<User?>? _streamSubscription;
+
+  @override
+  void initState() {
+    authChanges();
+    super.initState();
+  }
+
+  void authChanges() {
+    _streamSubscription ??= FirebaseAuth.instance.userChanges().listen((event) {
+      if (event != null) {
+        navigationKey.currentState!.pushAndRemoveUntil(
+            MaterialPageRoute(builder: (_) => const HomeScreen()),
+            (route) => false);
+      } else {
+        navigationKey.currentState!.pushAndRemoveUntil(
+            MaterialPageRoute(builder: (_) => OnboardScreen()),
+            (route) => false);
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) => const SplashLoad();
+}
+
+class SplashLoad extends StatelessWidget {
+  const SplashLoad({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
   }
 }
