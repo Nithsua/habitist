4ed6976f71b5bbd42edab2035baebc581341d7a9
Nivas Muthu M G <nivasmuthu452@gmail.com>
2022-10-08 13:20:02 +0530

    [UI]: Login Screen

--- lib/app/wrapper.dart
+++ lib/app/wrapper.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:habitist/app/screens/home.screen.dart';
+import 'package:habitist/app/screens/onboard.screen.dart';
 
 class Wrapper extends ConsumerWidget {
   const Wrapper({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return HomeScreen();
+    return OnboardScreen();
   }
 }
