1c19b3a50f6829cf6434c624fc331b518b54c7f0
Nivas Muthu M G <nivasmuthu452@gmail.com>
2022-10-08 09:39:32 +0530

    [RFCT]: Base for Navigation Bar

---
+++ lib/app/widgets/custom_bottom_navigaton_bar.widget.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:habitist/app/screens/new_habit.screen.dart';
+
+class CustomBottomNavigationBar extends StatelessWidget {
+  final void Function(int)? onTap;
+  final List<BottomNavigationBarItem> items;
+  const CustomBottomNavigationBar(
+      {Key? key, required this.onTap, required this.items})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: SizedBox(
+        height: 60,
+        child: Stack(
+          children: [
+            Center(
+              child: FloatingActionButton(
+                onPressed: () => Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => NewHabitScreen())),
+                backgroundColor: Colors.black,
+                child: const Icon(Icons.add),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
